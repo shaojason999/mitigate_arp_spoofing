7958265f977a4729ee565b225e59f59eb1448928
shaojason999 <ting2313@gmail.com>
2019-08-06 16:31:28 +0800

    Update

--- arp_spoofing.py
+++ arp_spoofing.py
@@ -6,7 +6,6 @@ from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.ofproto import ofproto_v1_3_parser
 from ryu.lib.packet import ethernet
-#from ryu.lib.packet import ether_types
 from ryu.lib.packet import arp, tcp, udp, dhcp, ipv4
 from ryu.lib.packet import packet
 
